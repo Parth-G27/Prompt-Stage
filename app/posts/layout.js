984592399f4d9d47088adb0806a64c96cb8e93cd
Parth <parthbidari13@gmail.com>
2023-05-31 04:31:43 +0530

    New-commit

---
+++ app/posts/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout() {
+  return (
+    <div>Nav to top</div>
+  )
+}
+
+export default layout
---
+++ app/posts/loading.js
@@ -0,0 +1,8 @@
+const Loading = () => {
+
+
+    // We can add UI in here to show skleton for loading 
+    
+    return <loadingSkleton />
+} 
+export default Loading
