9a52e9492beece23b2bc008f133132b3d77340f4
Parth <parthbidari13@gmail.com>
2023-06-04 13:25:50 +0530

    new

--- app/posts/layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function layout() {
-  return (
-    <div>Nav to top</div>
-  )
-}
-
-export default layout
--- app/posts/new/page.js
+++ app/posts/new/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>New post can be seen here</div>
+    <div>New post can be seen here, on this</div>
   )
 }
 
