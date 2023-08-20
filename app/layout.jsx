bfe8bdae3b9265bb5617dc44ffbd53e881bdbfc7
Parth <parthbidari13@gmail.com>
2023-08-20 20:11:32 +0530

    Working on Home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,10 @@
-// import React from 'react'
-import "@styles/globals.css";  // This will import the Tailwaind Css in the whole application 
+import "@styles/globals.css";
+// This will import the Tailwaind Css in the whole application 
 // import { children } from 'react';
 
 export const metadata = {
     title: 'Prompt Stage',
     description: 'Explore & Share prompts'
-
 }
 
 const Rootlayout = ({children}) => {
@@ -13,7 +12,7 @@ const Rootlayout = ({children}) => {
     <html lang="en">
         <body>
             <div className='main'>
-                <div className='gradiant'/>
+                <div className='gradient'/>
 
                 <main className='app'>
                     {children}
--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,18 @@
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
-        <div>One</div>
-        <div>two</div>
-        <div>three</div>
-        <div>four</div>
-
+    <section className="">
+      <h1 className="head_text text-center mb-3">Explore and Share Prompts 🤖
+      <br className="max-md:hidden"/>
+        <div className="purple_gradient text-center ">
+          Prompt Stage 
+        </div>
+      </h1>
+      <p className="text-center text-3xl font-medium py-5">
+        Prompt Stage is an open-source community platform for AI enthusiasts to Explore,
+         Share and Create innovative & artistic prompts. 
+      </p>
+      
     </section>
   )
 }
